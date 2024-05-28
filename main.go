bc40edb66be82190251077dc3886a5db042f93f6
harsh082ip <harshhvstech1975@gmail.com>
2024-05-28 17:17:01 +0530

    Added fan-out-fan-in pattern

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-	"time"
-
-	confinement "github.com/harsh082ip/Concurrency-go/goroutiine-confinement-pattern"
+	"fmt"
+	"log"
 )
+
+// "time"
+
+// confinement "github.com/harsh082ip/Concurrency-go/goroutiine-confinement-pattern"
+
+type Val interface {
+	int | int64 | uint32 | string | bool
+}
+
+func PrintVal[T Val](values T) {
+	var res T
+	println(res)
+	fmt.Println(res)
+	log.Println(res)
+}
 
 func main() {
 
 	// Confinement Pattern
 
-	for {
-		time.Sleep(time.Second / 2)
-		// confinement.Problem()
-		confinement.Solution()
-	}
+	// for {
+	// 	time.Sleep(time.Second / 2)
+	// 	// confinement.Problem()
+	// 	confinement.Solution()
+	// }
+	PrintVal(true)
+	// PrintVal(12)
+	println(-23)
 }
